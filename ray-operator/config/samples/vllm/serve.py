5ba336283e6a952e8c3539cf23120aa31bc205e5
mrjiangguoqing <mrjiangguoqing+2@gmail.com>
2024-11-18 08:12:27 +0800

    Update serve.py
    Signed-off-by: mrjiangguoqing <mrjiangguoqing+2@gmail.com>

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -114,8 +114,8 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     engine_args.worker_use_ray = True
 
     pg_resources = [{"CPU": 1, "GPU": 1}]
-    for i in range(3):
-        pg_resources.append({"CPU": 1, "GPU": 1})
+    #for i in range(3):
+        #pg_resources.append({"CPU": 1, "GPU": 1})
 
     return VLLMDeployment.options(placement_group_bundles=pg_resources, placement_group_strategy="STRICT_SPREAD").bind(
         engine_args,
