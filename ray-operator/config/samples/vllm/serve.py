0a46702065e6293e7d1a646552cdffce04ed3465
mrjiangguoqing <mrjiangguoqing+2@gmail.com>
2024-11-18 18:28:35 +0800

    Update serve.py
    Signed-off-by: mrjiangguoqing <mrjiangguoqing+2@gmail.com>

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -113,12 +113,13 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     engine_args = AsyncEngineArgs.from_cli_args(parsed_args)
     engine_args.worker_use_ray = True
     
-    pg_resources = [{"CPU": 1, "GPU": 4}]
+    #pg_resources = [{"CPU": 1, "GPU": 4}]
     #for i in range(3):
         #pg_resources.append({"CPU": 1, "GPU": 1},{"GPU":1},{"GPU":1})
     
-
-    return VLLMDeployment.options(placement_group_bundles=pg_resources, placement_group_strategy="SPREAD").bind(
+    #placement_group_bundles=pg_resources, placement_group_strategy="SPREAD"
+    
+    return VLLMDeployment.options().bind(
         engine_args,
         parsed_args.response_role,
         parsed_args.lora_modules,
