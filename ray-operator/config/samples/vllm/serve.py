05cbf0c5e3c7881ab0f4101ca95516d7bfbe1f7a
mrjiangguoqing <mrjiangguoqing+2@gmail.com>
2024-11-20 14:13:59 +0800

    Update serve.py
    Signed-off-by: mrjiangguoqing <mrjiangguoqing+2@gmail.com>

--- ray-operator/config/samples/vllm/serve.py
+++ ray-operator/config/samples/vllm/serve.py
@@ -126,7 +126,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
 
     #placement_group_bundles=bundles, placement_group_strategy="PACK"
 
-    bundles=[{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1}]
+    bundles=[{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1},{"CPU":1,"GPU": 1}]
     
     return VLLMDeployment.options(placement_group_bundles=bundles, placement_group_strategy="STRICT_SPREAD").bind(
         engine_args,
