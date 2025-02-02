ac060f070142815df9362d9dcb671e3eb788f8e5
mrjiangguoqing <mrjiangguoqing+2@gmail.com>
2025-02-02 09:27:40 +0800

    Update deepseek.py
    Signed-off-by: mrjiangguoqing <mrjiangguoqing+2@gmail.com>

--- ray-operator/config/samples/vllm/deepseek.py
+++ ray-operator/config/samples/vllm/deepseek.py
@@ -128,7 +128,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     #placement_group_bundles=bundles, placement_group_strategy="PACK"
 
     #bundles=[{"CPU":3,"GPU": 1},{"CPU":3,"GPU": 1}]
-    bundles=[{"CPU":3,"GPU": 1}]
+    bundles=[{"CPU":2,"GPU": 1}]
     
     return VLLMDeployment.options(placement_group_bundles=bundles, placement_group_strategy="STRICT_SPREAD").bind(
         engine_args,
