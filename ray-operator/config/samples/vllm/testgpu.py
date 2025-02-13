42719ca9bdebdc7a7bf60aa3b26eb619b7e66025
mrjiangguoqing <mrjiangguoqing+2@gmail.com>
2025-02-13 17:31:32 +0800

    Update testgpu.py
    Signed-off-by: mrjiangguoqing <mrjiangguoqing+2@gmail.com>

--- ray-operator/config/samples/vllm/testgpu.py
+++ ray-operator/config/samples/vllm/testgpu.py
@@ -146,7 +146,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
 
     #placement_group_bundles=bundles, placement_group_strategy="PACK"
 
-    bundles=[{"CPU":7,"GPU": 2},{"CPU":7,"GPU": 2}]
+    bundles=[{"CPU":7,"GPU": 1},{"CPU":7,"GPU": 1}]
     #bundles=[{"CPU":7,"GPU": 1}]
     
     return VLLMDeployment.options(placement_group_bundles=bundles, placement_group_strategy="STRICT_SPREAD").bind(
