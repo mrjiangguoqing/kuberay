4de272fa239c69f99f5bcd8e5415a5384c08fa74
mrjiangguoqing <mrjiangguoqing+2@gmail.com>
2025-02-14 13:53:35 +0800

    Update deepseek_70b.py
    Signed-off-by: mrjiangguoqing <mrjiangguoqing+2@gmail.com>

--- ray-operator/config/samples/vllm/deepseek_70b.py
+++ ray-operator/config/samples/vllm/deepseek_70b.py
@@ -155,7 +155,7 @@ def build_app(cli_args: Dict[str, str]) -> serve.Application:
     #placement_group_bundles=bundles, placement_group_strategy="PACK"
 
     #bundles=[{"CPU":8,"GPU": 1},{"CPU":7,"GPU": 1},{"CPU":0,"GPU": 1},{"CPU":0,"GPU": 1}]
-    bundles=[{"CPU":10,"GPU": 1},{"CPU":10,"GPU": 1},{"CPU":10,"GPU": 1},{"CPU":10,"GPU": 1}]
+    bundles=[{"CPU":30,"GPU": 1},{"CPU":30,"GPU": 1},{"CPU":30,"GPU": 1},{"CPU":30,"GPU": 1}]
     #bundles=[{"CPU":7,"GPU": 1}]
     
     return VLLMDeployment.options(placement_group_bundles=bundles, placement_group_strategy="SPREAD").bind(
